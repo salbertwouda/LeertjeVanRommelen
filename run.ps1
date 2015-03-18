96ea009868095f97eeaee71d0254add6c0909f82
Salbert Wouda <salbertwouda@gmail.com>
2015-03-19 00:07:27 +0100

    Add run.ps1

---
+++ run.ps1
@@ -0,0 +1,4 @@
+$source = ".\LeertjeVanRommelen\SampleData.csv"
+$target = "Data Source=.\SQLEXPRESS;Initial Catalog=opdracht;Integrated Security=True;"
+
+.\LeertjeVanRommelen\bin\Debug\LeertjeVanRommelen.exe -source $source-target $target
